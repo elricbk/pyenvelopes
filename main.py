f11e7a6e3dc837cf0aec10e00e88ce24c95403b4
Boris Kuchin <elricbk@gmail.com>
2016-01-05 15:29:49 +0300

    Logging to file in more convenient format;

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PySide.QtGui import QApplication
 import sys
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
+FORMAT = "%(asctime)s - %(levelname)-8s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, filename='/tmp/envelopes.log', format=FORMAT)
 
 app = QApplication(sys.argv)
 w = MainForm()
